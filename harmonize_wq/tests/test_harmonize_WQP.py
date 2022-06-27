909224e31b94bb4a06ee6864d204ee951a135043
Bousquin <Bousquin.Justin@epa.gov>
2022-06-27 15:56:30 -0500

    Updated tests for new lcoation module

--- harmonize_wq/tests/test_harmonize_WQP.py
+++ harmonize_wq/tests/test_harmonize_WQP.py
@@ -14,6 +14,7 @@ import os
 import pytest
 import geopandas
 import pandas
+from harmonize_wq import location
 from harmonize_wq import harmonize
 from harmonize_wq import convert
 from harmonize_wq import wrangle
@@ -166,7 +167,7 @@ def test_harmonize_locations():
     STATIONS : pandas.DataFrame
         Read from data/wqp_sites.txt.
     """
-    actual = harmonize.harmonize_locations(STATIONS)
+    actual = location.harmonize_locations(STATIONS)
 
     crs_col = 'HorizontalCoordinateReferenceSystemDatumName'
     expected_flag = crs_col + ': Bad datum OTHER, EPSG:4326 assumed'
