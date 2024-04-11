75cb7332c7f59880018ca3b6df23d6d8c204db5f
Bousquin <Bousquin.Justin@epa.gov>
2024-04-11 08:24:47 -0500

    remove try/except

--- harmonize_wq/__init__.py
+++ harmonize_wq/__init__.py
@@ -1,9 +1,5 @@
 from harmonize_wq import harmonize
-
-try:
-  from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-  from importlib_metadata import version, PackageNotFoundError
+from importlib.metadata import version, PackageNotFoundError
 
 try:
     __version__ = version('harmonize_wq')
