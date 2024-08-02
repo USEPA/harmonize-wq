559278df58cc57abbf8cd30e2e085327fb87b133
Bousquin <Bousquin.Justin@epa.gov>
2024-08-02 18:42:08 -0500

    '' -> ""

--- harmonize_wq/__init__.py
+++ harmonize_wq/__init__.py
@@ -3,6 +3,6 @@ from importlib.metadata import PackageNotFoundError, version
 from harmonize_wq import harmonize as harmonize
 
 try:
-    __version__ = version('harmonize_wq')
+    __version__ = version("harmonize_wq")
 except PackageNotFoundError:
     __version__ = "version-unknown"
