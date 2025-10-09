4d62b79043020240733bdd3ab02dc4273028a35c
Justin Bousquin <Bousquin.justin@epa.gov>
2025-10-09 16:35:06 -0500

    format

--- harmonize_wq/tests/test_harmonize_WQP.py
+++ harmonize_wq/tests/test_harmonize_WQP.py
@@ -36,8 +36,7 @@ ACTIVITIES = pandas.read_csv(os.path.join(test_dir, "wqp_activities.txt"))
 NARROW_RESULTS1 = pandas.read_csv(os.path.join(test_dir, "wqp_results1.txt"))
 # pH & Salinity
 NARROW_RESULTS2 = pandas.read_csv(
-    os.path.join(test_dir, "wqp_results2.txt"),
-    keep_default_na=False,
+    os.path.join(test_dir, "wqp_results2.txt"), keep_default_na=False,
     na_values = [""]
     )
 # Nitrogen & Conductivity
