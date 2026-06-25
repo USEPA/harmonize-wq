f20b368278bcf76331ecb0efbe17ccce99b9c3cb
Justin Bousquin <bousquin.justin@epa.gov>
2026-06-25 15:38:42 -0500

    Update DataFrame columns in wq_data.py
    [Revert] Add 'TOTAL PHOSPHORUS_ MIXED FORMS' to DataFrame columns.

--- harmonize_wq/wq_data.py
+++ harmonize_wq/wq_data.py
@@ -963,7 +963,8 @@ class WQCharData:
         >>> wq.df.columns
         Index(['CharacteristicName', 'ResultMeasure/MeasureUnitCode',
                'ResultMeasureValue', 'ResultSampleFractionText', 'Units', 'Phosphorus',
-               'QA_flag', 'TDP_Phosphorus', 'Other_Phosphorus'],
+               'QA_flag', 'TDP_Phosphorus', 'Other_Phosphorus',
+               'TOTAL PHOSPHORUS_ MIXED FORMS'],
               dtype='object')
         >>> wq.df[['TDP_Phosphorus',
         ...        'Other_Phosphorus'
