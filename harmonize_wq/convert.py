940e1c17a41cfe55c389fba974da3dcf2be41d67
Justin Bousquin <bousquin.justin@epa.gov>
2026-06-25 13:32:05 -0500

    Refactor doctest for convert_unit_series function
    Updated doctest to normalize whitespace and removed unnecessary comment.

--- harmonize_wq/convert.py
+++ harmonize_wq/convert.py
@@ -95,11 +95,10 @@ def convert_unit_series(quantity_series, unit_series, units, ureg=None, errors="
     Convert series to series of pint Quantity objects in 'mg/l':
 
     >>> from harmonize_wq import convert
-    >>> convert.convert_unit_series(quantity_series, unit_series, units = 'mg/l')
+    >>> convert.convert_unit_series(quantity_series, unit_series, units = 'mg/l') # doctest: +NORMALIZE_WHITESPACE
     0                   1.0 milligram / liter
     1    10000.0... milligram / liter
     dtype: object
-    # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
     """
     if quantity_series.dtype == "O":
         quantity_series = pandas.to_numeric(quantity_series)
