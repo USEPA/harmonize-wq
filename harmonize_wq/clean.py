a7ba51d8256f4f10389323cd024e387bc32d3461
Bousquin <Bousquin.Justin@epa.gov>
2026-07-09 08:48:24 -0500

    Add QA flag error if mask type is not pandas series. This is a rough debug to identify where it is being set with the incorrect type

--- harmonize_wq/clean.py
+++ harmonize_wq/clean.py
@@ -348,6 +348,8 @@ def add_qa_flag(df_in, mask, flag):
     1         Phosphorus              0.265    <NA>
     2             Carbon                2.1   words
     """
+    if not isinstance(mask, pandas.Series):
+      raise TypeError(f"mask must be a pandas Series not {type(mask)}. {mask}")
     df_out = df_in.copy()
     if "QA_flag" not in list(df_out.columns):
         df_out["QA_flag"] = nan
